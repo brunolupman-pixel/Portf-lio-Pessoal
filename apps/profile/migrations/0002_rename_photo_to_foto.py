0b3bbe2f6fca8f0cf610a5da2413d3a3d9c8d258
Bruno L. Mancinelli <brunolupman@gmail.com>
2026-03-01 22:06:11 -0300

    Rename media fields and update templates

---
+++ apps/profile/migrations/0002_rename_photo_to_foto.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profile', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='profile',
+            old_name='photo',
+            new_name='foto',
+        ),
+    ]
